a964b809fba5acb2ae76158faaf4dae6c9082b52
rukia <ruqayashar1@gmail.com>
2023-06-23 15:22:41 +0300

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+        validates :title, presence: true
+        validates :content, length: { minimum: 250 }
+        validates :summary, length: { maximum: 250 }
+        validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+        validate :clickbait?
+      
+        private
+      
+        def clickbait?
+          clickbait_keywords = [
+            /Won't Believe/i,
+            /Secret/i,
+            /^Top \d/i,
+            /Guess/i
+          ]
+      
+          if clickbait_keywords.none? { |pattern| pattern.match?(title) }
+            errors.add(:title, 'must be clickbait')
+          end
+        end
+      
 end
